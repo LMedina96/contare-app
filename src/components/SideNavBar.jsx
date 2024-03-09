4e4a68b5728534a484a55fc826f031cf4f079d68
Lucas Medina <lucasgmedina96@gmail.com>
2024-03-09 12:33:05 -0300

    side bar modified

--- src/components/SideNavBar.jsx
+++ src/components/SideNavBar.jsx
@@ -3,7 +3,7 @@ import { Icon } from '@iconify/react';
 
 const SideNavBar = () => {
 
-    const [itemSelected, setItemSelected] = useState(null)
+    const [itemSelected, setItemSelected] = useState(0)
 
     const handleItemSelected = (index) => {
         setItemSelected(index)
@@ -11,19 +11,22 @@ const SideNavBar = () => {
 
     const listItem = [
         'carbon:dashboard',
+        'material-symbols:payments-outline',
         'mdi:finance',
-        'ph:calculator'
     ]
 
     return (
-        <div className="fixed left-4 top-1/2 transform -translate-y-1/2 bg-white rounded-xl shadow-md">
-            <ul className='my-1'>
-
+        <div className="fixed left-4 top-1/2 transform -translate-y-1/2 border border-slate-600 rounded-full shadow-xl">
+            <ul>
                 {
                     listItem.map((item, index) => (
                         <li key={index}>
                             <button
-                                className={`p-4 hover:bg-gray-200 active:bg-gray-300 ${itemSelected === index ? 'bg-gray-300' : ''}`}
+                                className={
+                                    `p-4 hover:bg-gray-200 active:bg-gray-300 
+                                    ${itemSelected === index ? 'bg-gray-300' : ''} 
+                                    ${index === 0 ? 'rounded-t-full' : index === 2 ? 'rounded-b-full' : ''}`
+                                }
                                 onClick={() => handleItemSelected(index)}
                             >
                                 <Icon icon={item} width="24" height="24" />
